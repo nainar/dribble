7c97dead08a6ceb524c9e97c258b8224fa4b8a3d
Naina Raisinghani <nainar@chromium.org>
2016-02-20 12:31:00 +1100

    add and setup voting

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   resources :posts do
   	member do
-  		# get "like", to: "posts#upvote"
-  		# get "dislike", to: "posts#downvote"
+  		get "like", to: "posts#upvote"
+  		get "dislike", to: "posts#downvote"
   	end
   	resources :comments
 	end
